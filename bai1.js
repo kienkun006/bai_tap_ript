841d25b6690758a2dc7af718a7fdfa1153653a32
KIEU TRUNG KIEN <kieutrungkien1382006@gmail.com>
2025-03-22 07:25:40 +0700

    Add files via upload

--- bai1.js
+++ bai1.js
@@ -1,17 +1,16 @@
 let n = parseFloat(prompt("Nhập điểm:"));
 
-if (n >= 9) {
+if (n >= 9 && n <=10) {
     console.log("Xuất sắc");
-} else if (n >= 8) {
+} else if (n >= 8 && n<9)  {
     console.log("Giỏi");
-} else if (n >= 6.5) {
+} else if (n >= 6.5 && n<8) {
     console.log("Khá");
-} else if (n >= 5) {
+} else if (n >= 5 && n<6.5) {
     console.log("Trung bình");
-} else {
+} else if (n >=0 && n <5){
     console.log("Yếu");
+} else {
+    console.log("Lỗi")
 }
-
-
-
 
